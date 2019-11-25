f41d4ea69a7c42a9a63893d819c8aea5919b04a7
Aurelio Guerrero <aurelio.guerrero@gmail.com>
2019-11-24 23:11:13 -0500

    Segunda prueba de uso del repositorio

--- MyMedicalAppointments/src/Main.java
+++ MyMedicalAppointments/src/Main.java
@@ -1,7 +1,6 @@
 import static ui.UIMenu.*;
 
 public class Main {
-    //Prueba de uso de repositorio
     public static void main(String[] args) {
         Doctor myDoctor = new Doctor();
         myDoctor.name = "Alejandro Rodriguez";
