db0e7f879b59a5dc8550fe82d2949ce5f238e3cc
Aurelio Guerrero <aurelio.guerrero@gmail.com>
2019-11-25 23:05:34 -0500

    JAVA SE orientado a objetos: sobrecarga de métodos y contructor

--- MyMedicalAppointments/src/Main.java
+++ MyMedicalAppointments/src/Main.java
@@ -1,17 +1,12 @@
+import javax.print.Doc;
+
 import static ui.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor();
-        myDoctor.name = "Alejandro Rodriguez";
-        myDoctor.showName();
-        myDoctor.showId();
-        System.out.println(Doctor.id);
-
-        Doctor myDoctorAnn = new Doctor();
-        myDoctorAnn.showId();
-        System.out.println(Doctor.id);
-
-        showMenu();
+        //showMenu();
+        Doctor myDoctor = new Doctor("Aurelio Guerrero","Pediatría");
+        System.out.println(myDoctor.name);
+        System.out.println(myDoctor.speciality);
     }
 }
