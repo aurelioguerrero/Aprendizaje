0015734e62e6ef45f9ee20d4437d6926435b19c8
Aurelio Guerrero <aurelio.guerrero@mvm.com.co>
2019-11-26 14:41:45 -0500

    JAVA SE Orientado a objetos - Modificadores de acceso

--- MyMedicalAppointments/src/Main.java
+++ MyMedicalAppointments/src/Main.java
@@ -8,5 +8,12 @@ public class Main {
         Doctor myDoctor = new Doctor("Aurelio Guerrero","Pediatría");
         System.out.println(myDoctor.name);
         System.out.println(myDoctor.speciality);
+
+        Patient patient = new Patient("Alejandra","alejandra@gmail.com");
+        patient.setWeight(54.6);
+        System.out.println(patient.getWeight());
+
+        patient.setPhoneNumber("12345678");
+        System.out.println(patient.getPhoneNumber());
     }
 }
