ea35022c95bb825391a95966482eb8d3985de1ea
Aurelio Guerrero <aurelio.guerrero@gmail.com>
2019-12-02 22:55:55 -0500

    JAVA SE orientado a objetos - Clases internas y locales a un método

--- MyMedicalAppointments/src/Main.java
+++ MyMedicalAppointments/src/Main.java
@@ -1,19 +1,26 @@
 import javax.print.Doc;
 
+import java.util.Date;
+
 import static ui.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
         //showMenu();
         Doctor myDoctor = new Doctor("Aurelio Guerrero","Pediatría");
-        System.out.println(myDoctor.name);
-        System.out.println(myDoctor.speciality);
+        myDoctor.addAvailableAppointment(new Date(),"4pm");
+        myDoctor.addAvailableAppointment(new Date(),"10am");
+        myDoctor.addAvailableAppointment(new Date(),"1pm");
 
-        Patient patient = new Patient("Alejandra","alejandra@gmail.com");
+        for (Doctor.AvailableAppointment aA:myDoctor.getAvailableAppointments()) {
+            System.out.println(aA.getDate()+" "+aA.getTime());
+        }
+
+        /*Patient patient = new Patient("Alejandra","alejandra@gmail.com");
         patient.setWeight(54.6);
         System.out.println(patient.getWeight());
 
         patient.setPhoneNumber("12345678");
-        System.out.println(patient.getPhoneNumber());
+        System.out.println(patient.getPhoneNumber());*/
     }
 }
